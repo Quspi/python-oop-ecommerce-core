06d33bae70d8f1251431aab3c40079ab57df1279
Oleg Tamanov <olegtamanov@gmail.com>
2026-01-21 16:12:17 +0300

    add fixtures for test Category class

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 
+from src.category import Category
 from src.product import Product
 
 
@@ -11,3 +12,25 @@ def product_1():
 @pytest.fixture
 def product_2():
     return Product("Iphone 15", "512GB, Gray space", 210000.0, 18)
+
+
+@pytest.fixture
+def category(product_1, product_2):
+    Category.category_count = 0
+    Category.product_count = 0
+    return Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни",
+        [product_1, product_2],
+    )
+
+
+@pytest.fixture
+def empty_category():
+    Category.category_count = 0
+    Category.product_count = 0
+    return Category(
+        "Телевизоры",
+        "Современные телевизоры",
+        [],
+    )
