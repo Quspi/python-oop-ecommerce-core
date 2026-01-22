b1d277691c72ad554d892115ca0aaab0afdbbc59
Oleg Tamanov <olegtamanov@gmail.com>
2026-01-22 18:54:50 +0300

    fix create_objects_from_json test for products property

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -37,11 +37,12 @@ def test_create_objects_from_json(json_data):
     for category in result:
         assert isinstance(category, Category)
 
-        for product in category.products:
-            assert isinstance(product, Product)
+        products_str = category.products
+        assert isinstance(products_str, str)
+        assert len(products_str) > 0
 
+    assert "100.0" in result[0].products
     assert result[0].name == "Смартфоны"
-    assert result[0].products[0].price == 100.0
 
 
 def test_invalid_data_create_objects_from_json(invalid_data):
