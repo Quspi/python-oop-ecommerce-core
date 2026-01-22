0b274ad89751327732ecde676305b93a6cbba53d
Oleg Tamanov <olegtamanov@gmail.com>
2026-01-22 17:49:16 +0300

    add new_product classmethod to create Product from dict

--- src/product.py
+++ src/product.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+
 class Product:
     """Класс для представления товара."""
 
@@ -12,3 +15,11 @@ class Product:
         self.description = description
         self.price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, product: dict) -> Product:
+        """Метод для создания объекта Product из словаря."""
+        try:
+            return cls(**product)
+        except TypeError:
+            raise ValueError("Ошибка в структуре данных продукта")
