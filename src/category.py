133495c72ac120b6a5a278ab56d5d6cd5b2f5c84
Oleg Tamanov <olegtamanov@gmail.com>
2026-01-24 19:13:52 +0300

    add __str__ method for Category

--- src/category.py
+++ src/category.py
@@ -20,6 +20,14 @@ class Category:
         Category.category_count += 1
         Category.product_count += len(self.__products)
 
+    def __str__(self) -> str:
+        """Возвращает строку: 'название категории, количество продуктов:
+           X шт.', где X - сумма quantity всех товаров категории."""
+        total_products = 0
+        for product in self.__products:
+            total_products += product.quantity
+        return f"{self.name}, количество продуктов: {total_products} шт."
+
     def add_product(self, product: Product) -> None:
         """Метод для добавления продукта в список продуктов категории."""
         if isinstance(product, Product):
@@ -33,3 +41,16 @@ class Category:
         for product in self.__products:
             products_info += str(product)
         return products_info
+
+
+if __name__ == "__main__":
+    print(
+        Category(
+            "Смартфоны",
+            "Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни",
+            [
+                Product("Samsung Galaxy C23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 10),
+                Product("Iphone 15", "512GB, Gray space", 210000.0, 18),
+            ],
+        )
+    )
